745ae9757a958680f5d1677d52601619449d2e29
Ivad <yvesyvad@gmail.com>
2023-12-13 16:55:26 +0200

    feat: setup tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import './assets/main.css'
+import App from "@/App.vue";
+import { createApp } from "vue";
+import "./index.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+createApp(App).mount("#app");
