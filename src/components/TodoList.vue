6d9615e319ee3e9d88bc72509a4a2fbf1fd90345
Ivad <yvesyvad@gmail.com>
2023-12-18 13:23:32 +0200

    feat: trigger deployment

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -10,7 +10,7 @@ defineEmits<{
 
 <template>
   <ul>
-    <li v-for="todo in todos" :key="todo.id" class="flex py-4 border-b gap-7 items-enter">
+    <li v-for="todo in todos" :key="todo.id" class="flex py-4 border-b gap-8 items-enter">
       <input
         :checked="todo.completed"
         @change="$emit('checkTodo', todo.id)"
